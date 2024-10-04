6b480a80ba0a0a32337aa0214b8b46cb20717596
devdeer-nick <nick.sidenko@outlook.com>
2024-10-04 16:06:02 +0200

    Created a todo model

---
+++ src/Logic/Logic.Models/TodoItemModel.cs
@@ -0,0 +1,39 @@
+﻿namespace devdeer.ListOfWork.Logic.Models
+{
+    /// <summary>
+    /// A representation of a single todo list item inside of the backend.
+    /// </summary>
+    public class TodoItemModel
+    { 
+        /// <summary>
+        /// The unique identifier of the item in the backend.
+        /// </summary>
+        public string Id { get; } = Guid.NewGuid()
+            .ToString("N")
+            .Substring(1, 4);
+        /// <summary>
+        /// The title of the todo item.
+        /// </summary>
+        public string Title { get; set; } = default!;
+        /// <summary>
+        /// A short description of the todo.
+        /// </summary>
+        public string Description { get; set; } = default!;
+        /// <summary>
+        /// The time of the item creation. The time is automatically set to now to display the time of the request.
+        /// </summary>
+        public DateTimeOffset CreatedTime => DateTimeOffset.Now;
+        /// <summary>
+        /// The time when the todo is supposed to be fulfilled.
+        /// </summary>
+        public DateTimeOffset DueTime { get; set; }
+        /// <summary>
+        /// The time of the todo completion. The property indicates if the todo is fulfilled and at what time.
+        /// </summary>
+        public DateTimeOffset? DoneAt { get; set; }
+        /// <summary>
+        /// The indicator of the todo presence in the backend. If the property is set to false, then the todo was deleted.
+        /// </summary>
+        public bool IsDeleted { get; set; } = false;
+    }
+}
