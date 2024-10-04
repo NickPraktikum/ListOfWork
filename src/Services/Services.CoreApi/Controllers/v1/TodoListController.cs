4e558085faa4d3e852915a95de2039fdba15649c
devdeer-nick <nick.sidenko@outlook.com>
2024-10-04 15:21:14 +0200

    Created a simple Api solution

---
+++ src/Services/Services.CoreApi/Controllers/v1/TodoListController.cs
@@ -0,0 +1,11 @@
+﻿namespace devdeer.ListOfWork.Services.CoreApi.Controllers.v1
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoListController : ControllerBase
+    {
+        public TodoListController() { }
+    }
+}
