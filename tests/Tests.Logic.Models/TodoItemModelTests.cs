49ae27a910cfa160f5059940511693f634be4b6d
devdeer-nick <nick.sidenko@outlook.com>
2024-10-07 12:44:28 +0200

    Created a simple template for unit testing of the models

---
+++ tests/Tests.Logic.Models/TodoItemModelTests.cs
@@ -0,0 +1,16 @@
+﻿namespace devdeer.ListOfWork.Tests.Logic.Models
+{
+    public class TodoItemModelTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
