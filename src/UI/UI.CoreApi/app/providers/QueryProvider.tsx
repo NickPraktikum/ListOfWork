ed49e575f821c0c4065bb59d1dde2a819c655c51
devdeer-nick <nick.sidenko@outlook.com>
2024-10-08 22:29:00 +0200

    Fixed the issue with react-query devtools

---
+++ src/UI/UI.CoreApi/app/providers/QueryProvider.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const client = new QueryClient();
+
+function QueryProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
+}
+export { QueryProvider };
